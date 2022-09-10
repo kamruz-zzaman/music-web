0e4c5b91e04eeb9bc0537826b31fcc4b87e2c6a2
Md Nazmul Islam <86624274+kamruz-zzaman@users.noreply.github.com>
2022-09-11 01:08:16 +0600

    Folder Structure Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import FavouritesMusic from './components/FavouritesMusic/FavouritesMusic';
+import Login from './components/Login/Login';
+import Playlist from './components/Playlist/Playlist';
+import Home from './pages/HomePage/Home';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="login" element={<Login/>} />
+        <Route path="favourites_music" element={<FavouritesMusic/>} />
+        <Route path="playlist" element={<Playlist/>} />
+      </Routes>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <>
+      <div  className='h-100 bg-info'>
+        <div>asdsadsfdfd</div>
+        <div></div>
+      </div>
+      
+    </>
+    )
+}
